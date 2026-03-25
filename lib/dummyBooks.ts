8163b4b6dadbb1fb0fcdd3e06aa234706737567e
kabelo385 <kabeloxabendlini385@gmail.com>
2026-03-25 12:18:18 +0200

    fix: remove .env.example from tracking

---
+++ lib/dummyBooks.ts
@@ -0,0 +1,27 @@
+export type Book = {
+  id: string;
+  title: string;
+  img: string;
+  link: string;
+};
+
+export const dummyBooks: Book[] = [
+  {
+    id: "dummy-1",
+    title: "MERN Stack",
+    link: "https://www.amazon.com/Beginning-MERN-Stack-MongoDB-Express/dp/B0979MGJ5J",
+    img: "https://m.media-amazon.com/images/I/41y8qC9RT0S._SX404_BO1,204,203,200_.jpg",
+  },
+  {
+    id: "dummy-2",
+    title: "Beginning GraphQL",
+    link: "https://www.amazon.com/Beginning-GraphQL-React-NodeJS-Apollo/dp/B0BXMRB5VF/",
+    img: "https://m.media-amazon.com/images/I/41+PG6uPdHL._SX404_BO1,204,203,200_.jpg",
+  },
+  {
+    id: "dummy-3",
+    title: "Beginning React Hooks",
+    link: "https://www.amazon.com/Beginning-React-Hooks-Greg-Lim/dp/B0892HRT3C/",
+    img: "https://m.media-amazon.com/images/I/41e9U1d9QIL._SX404_BO1,204,203,200_.jpg",
+  },
+];
